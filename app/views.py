7d1db3f3ec3c5014ea3a580df431428a124c78a1
Eston Kagwima <ekagwima745@gmail.com>
2021-04-17 00:08:49 +0300

    adding index and sources templates

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from app import app
+
+#Views
+@app.route('/')
+def index ():
+  '''
+  View root page function returns the index page
+  '''
+  return render_template('index.html')
+
+@app.route('/sources/<sources_id>')
+def sources(sources_id):
+  '''
+  View sources page function that returns the sources and details page
+  '''
+  return render_template('sources.html',id = sources_id)
